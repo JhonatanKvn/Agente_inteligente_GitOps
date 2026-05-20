fe09bbcb18cac300a095f6e12e91fe7826cae814
Jhonatan Castillo <jkcastillo93@ucatolica.edu.co>
2026-05-19 20:06:50 -0500

    chore: eliminar rastro de carpeta intermedia del indice

--- agente_tesis/app_flask.py
+++
@@ -1,9 +0,0 @@
-"""Compatibilidad: punto de entrada antiguo.
-
-Ejecuta la app Flask delegando al nuevo modulo organizado.
-"""
-
-from app.web.server import run, app
-
-if __name__ == "__main__":
-    run()
